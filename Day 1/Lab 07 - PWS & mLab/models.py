dc029fa3bf9fba6d69b0a8fe2afaee93d4f0b37c
Theo Crithary <theo@apacit.com.au>
2020-02-17 14:19:57 +1100

    .

--- Day 1/Lab 07 - PWS & mLab/models.py
+++ Day 1/Lab 07 - PWS & mLab/models.py
@@ -11,7 +11,7 @@ import os
 import boto3
 import json
 from pymongo import MongoClient
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from PIL import Image
 from config import ecs_test_drive
 
@@ -22,7 +22,7 @@ if 'VCAP_SERVICES' in os.environ:
     client = MongoClient(MONGOCRED["uri"])
     DB_NAME = str(MONGOCRED["uri"].split("/")[-1])
 
-# Otherwise, assume running locally with local MongoDB instance    
+# Otherwise, assume running locally with local MongoDB instance
 else:
     client = MongoClient('127.0.0.1:27017')
     DB_NAME = "mongodb"  ##### Make sure this matches the name of your MongoDB database ######
@@ -51,10 +51,10 @@ def insert_photo(request):
 def upload_photo(file):
     # Get ECS credentials from external config file
     ecs_endpoint_url = ecs_test_drive['ecs_endpoint_url']
-    ecs_access_key_id = ecs_test_drive['ecs_access_key_id']  
+    ecs_access_key_id = ecs_test_drive['ecs_access_key_id']
     ecs_secret_key = ecs_test_drive['ecs_secret_key']
     ecs_bucket_name = ecs_test_drive['ecs_bucket_name']
- 
+
     # Open a session with ECS using the S3 API
     session = boto3.resource(service_name='s3', aws_access_key_id=ecs_access_key_id, aws_secret_access_key=ecs_secret_key, endpoint_url=ecs_endpoint_url)
 
@@ -72,7 +72,7 @@ def upload_photo(file):
         thumbfile = filename.rsplit(".",1)[0] + "-thumb.jpg"
         img.save("uploads/" + thumbfile,"JPEG")
         img.close()
-    
+
     # Empty the variables to prevent memory leaks
     img = None
 
