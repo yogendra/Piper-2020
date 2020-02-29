417c1b149939d80467154a76e7584124d8052eb4
Theo Crithary <theo@apacit.com.au>
2020-02-29 17:33:17 +1100

    .

--- Day 2/Lab 01 - Docker/app.py
+++ Day 2/Lab 01 - Docker/app.py
@@ -23,4 +23,4 @@ def index():
 
 ##### Run the Flask instance, browse to http://<< Host IP or URL >> #####
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
