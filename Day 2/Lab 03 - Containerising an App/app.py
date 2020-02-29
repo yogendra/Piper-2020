62765879ccc4ec54d54fb09d9bec4a99d12c9907
Theo Crithary <theo@apacit.com.au>
2020-02-29 18:20:17 +1100

    .

--- Day 2/Lab 03 - Containerising an App/app.py
+++ Day 2/Lab 03 - Containerising an App/app.py
@@ -33,4 +33,4 @@ def photo(photo):
 
 ##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
 if __name__ == "__main__":
-	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '80')), threaded=True)
+	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
